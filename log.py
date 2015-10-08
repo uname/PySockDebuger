451aff9b5a5ab4db2c52d7be3d0ee32c534e10df
uname <529149237@qq.com>
2015-10-08 20:53:45 +0800

    up

--- log.py
+++ log.py
@@ -1,14 +1,18 @@
 #-*- coding: utf-8 -*-
+import config
 import logging
 import sys
 
-logger = logging.getLogger("")
+logger = logging.getLogger("root")
 __formatter = logging.Formatter("[%(levelname)-7s][%(asctime)s][%(filename)s:%(lineno)d] %(message)s", "%d %b %Y %H:%M:%S")
-__streamHandler = logging.StreamHandler(sys.stderr)
-__streamHandler.setFormatter(__formatter)
+
+if config.RELEASE_TYPE == "py":
+    __streamHandler = logging.StreamHandler(sys.stdout)
+    __streamHandler.setFormatter(__formatter)
+    logger.addHandler(__streamHandler)
+    
 __fileHandler = logging.FileHandler("debug.log")
 __fileHandler.setFormatter(__formatter)
+logger.addHandler(__fileHandler)
 
 logger.setLevel(logging.DEBUG)
-logger.addHandler(__streamHandler)
-logger.addHandler(__fileHandler)
